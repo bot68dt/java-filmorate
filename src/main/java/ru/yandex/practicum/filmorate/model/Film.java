a9676cb65036f99242a7981a064ad08c0c4111b9
dt68 <destruct.you.down@gmail.com>
2025-01-04 13:44:04 +0300

    1

--- src/main/java/ru/yandex/practicum/filmorate/model/Film.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/Film.java
@@ -1,5 +1,24 @@
 package ru.yandex.practicum.filmorate.model;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NonNull;
 
+import java.time.LocalDate;
+
+@Data
+@EqualsAndHashCode(of = { "id" })
+@AllArgsConstructor(staticName = "of")
 public class Film {
+    private Long id;
+    @NonNull
+    @NotBlank
+    private String name;
+    private String description;
+    @JsonFormat(pattern = "dd.MM.yyyy")
+    private LocalDate releaseDate;
+    private Integer duration;
 }
